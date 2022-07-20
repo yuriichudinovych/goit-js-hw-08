0b0cca6be6fcbbe4904295097c2090b12eae8424
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-07-20 20:48:30 +0300

    adding task two

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const throttledOnTimeUpdate = throttle(getTimeupdateData, 1000);
+player.on('timeupdate', throttledOnTimeUpdate);
+
+function getTimeupdateData(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  console.log(data);
+}
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player
+  .setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+  )
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
